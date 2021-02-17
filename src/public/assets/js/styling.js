d6a19ca0345b28ecdc1cce66f149cbb453e9a093
Jude Beadle <info@jbeadle.de>
2021-02-17 18:25:22 +0100

    tidying up code

--- src/public/assets/js/styling.js
+++ src/public/assets/js/styling.js
@@ -1,47 +1,32 @@
-// non functional enhancements
+// Seperation of concerns:
+// this JS is purely for non functional enhancements, such as animation of DOM elements
 
-
-// landing on Mars -> zooming on svg depending on scrolling down to first text section #intro
+// grabbing DOM Elements that will be styled or that help calculate stuff
 const intro = document.getElementById("intro");
 const mars = document.getElementById("mars-graphic");
 const scrollInstructions = document.getElementById("hero").getElementsByClassName("instructions")[0];
+
+// initial values - these will be different each time depending on device
 const onePercent = intro.getBoundingClientRect().top * 0.01;
 
-
-/*let BGPosition = 0;
-let resetSize = 400;
-function scrollBodyBG() {
-    BGPosition = ++BGPosition % resetSize; // this counts up to 400 and then starts again. maybe use for texture position ?
-    //$('body').css("background-position", (-1 * BGPosition) + "px 0px");
-    //console.log(BGPosition);
-}*/
-
-
-
-const doStuffOnInterval = () => {
+// landing on Mars -> zooming on svg depending on scrolling down to first text section #intro
+const zoomScroll = () => {
 	const maxSidepadding = 0.5; // vw => 1 = 100%
 	let sidepadding = maxSidepadding * ( intro.getBoundingClientRect().top / onePercent );
-	console.log(sidepadding);
   let padding = "0px " + sidepadding + "vw";
-  console.log(padding);
-  console.log(onePercent);
   mars.style.padding = padding;
-  //mars.style.backgroundColor = "red"
+  scrollInstructions.style.opacity = (( Math.floor( intro.getBoundingClientRect().top ) / ( onePercent * 0.5 ) ) / 100) - 1 ;
   if (sidepadding <= 0) {
-    stopInterval(doStuffOnInterval)
+    stopInterval(zoomScroll)
   }
 }
 
+// helper function to stop
 const stopInterval = (interval) => {
   clearInterval(interval);
 }
 
-
-
-
-
-// listening for load event because page should load before any JS is called
+// listening for load event because page should load before we can calculate div heights etc
 window.addEventListener('load', () => {
-	console.log(mars);
- 	const interval = setInterval(doStuffOnInterval, 50);
+ 	const interval = setInterval(zoomScroll, 50);
 })
