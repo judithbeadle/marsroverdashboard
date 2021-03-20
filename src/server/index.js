ac70ec386288e5070213e43ece3aa433958cc61f
Jude Beadle <info@jbeadle.de>
2021-03-20 10:55:07 +0100

    make it better

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const Immutable = require('immutable');
 
